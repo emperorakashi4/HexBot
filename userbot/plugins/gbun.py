f623c5c4b830d66c76e99576ac67e7ce8c8ae53d
emperorakashi4 <63154092+emperorakashi4@users.noreply.github.com>
2020-04-20 08:02:55 +0530

    Update gbun.py

--- userbot/plugins/gbun.py
+++ userbot/plugins/gbun.py
@@ -14,7 +14,7 @@ async def gbun(event):
     gbunVar = gbunVar[6:]
     mentions = "`Alert! User Is Banned In The` @Xiaomeme `Federation\n`"
     no_reason = "**Reason:** `Likely A Spammer!`"
-    await event.edit("**Starting A Federation Ban!")
+    await event.edit("**Starting A Federation Ban!**")
     asyncio.sleep(4)
     chat = await event.get_input_chat()
     async for x in borg.iter_participants(chat, filter=ChannelParticipantsAdmins):
